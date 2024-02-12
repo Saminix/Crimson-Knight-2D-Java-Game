ec0b21dfaed74870b4a6f5462fa18b08f3bd7d90
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-02-12 13:20:32 +0000

    working on game

--- src/game/Game.java
+++ src/game/Game.java
@@ -25,29 +25,51 @@ public class Game {
 
         //2. populate it with bodies (ex: platforms, collectibles, characters)
 
+        //make a character (with an overlaid image)
+        Shape warriorShape = new BoxShape(2,4);
+        DynamicBody warrior = new DynamicBody(world, warriorShape);
+        warrior.setPosition(new Vec2(6,-12.5f));
+        warrior.addImage(new BodyImage("data/warrior.gif", 9.5f));
+
         //make a ground platform
-        Shape shape = new BoxShape(30, 0.5f);
+        Shape shape = new BoxShape(30, 0.9f);
+        //measure width and height for the block
         StaticBody ground = new StaticBody(world, shape);
-        ground.setPosition(new Vec2(0f, -11.5f));
+        ground.setPosition(new Vec2(5f, -12.5f));
+        ground.addImage(new BodyImage("data/grassfloor.png", 5));
+        //y(-5.5f) = y coordinate
+        //x(0f) = x coordinate
 
         // make a suspended platform
-        Shape platformShape = new BoxShape(3, 0.5f);
-        StaticBody platform1 = new StaticBody(world, platformShape);
-        platform1.setPosition(new Vec2(-8, -4f));
+        Shape platformShape1 = new BoxShape(3, 0.5f);
+        //create another block( in the air) and measure out length and height
+        StaticBody platform1 = new StaticBody(world, platformShape1);
+        // call this static block platform 1 and populate into world
+        platform1.setPosition(new Vec2(-9, -6f));
+        platform1.addImage(new BodyImage("data/grassplatform.png", 5));
+        //initialise the coordinates of the block - note this is the centre of coordinates
 
-        //make a character (with an overlaid image)
-        Shape studentShape = new BoxShape(1,2);
-        DynamicBody student = new DynamicBody(world, studentShape);
-        student.setPosition(new Vec2(4,-5));
-        student.addImage(new BodyImage("data/student.png", 4));
+        Shape platformShape2 = new BoxShape(3, 0.5f);
+        StaticBody platform2 = new StaticBody(world, platformShape2);
+        platform2.setPosition(new Vec2(3,5));
+        platform2.addImage(new BodyImage("data/grassplatform.png", 6));
+
+
+       
+
+
+
+
+
+
 
 
         //3. make a view to look into the game world
-        UserView view = new UserView(world, 500, 500);
+        UserView view = new UserView(world, 600, 500);
 
 
         //optional: draw a 1-metre grid over the view
-        // view.setGridResolution(1);
+         view.setGridResolution(1);
 
 
         //4. create a Java window (frame) and add the game
