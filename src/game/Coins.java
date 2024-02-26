a91507961c927410fcb3f7ad9663119e9cdbea97
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-02-26 15:28:48 +0000

    Created a coin gif in game extending dynamic body and adding image to the world to implement a point system with this interface

---
+++ src/game/Coins.java
@@ -0,0 +1,19 @@
+package game;
+import city.cs.engine.*;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+import city.cs.engine.DynamicBody;
+
+public class Coins extends DynamicBody {
+    private static final Shape coinsShape = new CircleShape(1);
+
+    private static final BodyImage image = new BodyImage("data/Coins.gif", 2f);
+
+
+    public Coins(World w){
+        super(w, coinsShape);
+        addImage(image);
+    }
+}
