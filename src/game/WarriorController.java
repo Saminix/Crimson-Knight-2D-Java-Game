729ad26c42afb14468ef428232e5ca4ed58aa51e
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-02-27 00:51:46 +0000

    allowing the Warrior when placed left or right depending on a and d keys to be facing the direction an attack with the sword occurs. Using a boolean value to put facing right true the image facing the right will attack right and image facing left will attack left based on position of character

--- src/game/WarriorController.java
+++ src/game/WarriorController.java
@@ -9,6 +9,8 @@ import java.awt.event.KeyListener;
 public class WarriorController implements KeyListener {
     private Warrior warrior;
 
+    private boolean isFacingRight = true;
+
     public WarriorController(Warrior warrior) {
         this.warrior = warrior;
     }
@@ -25,15 +27,26 @@ public class WarriorController implements KeyListener {
         if (code == KeyEvent.VK_A) {
             warrior.startWalking(-5);
             warrior.removeAllImages();
-            warrior.addImage(new BodyImage("data/WarriorLeft.gif", 9.5f));
+            warrior.addImage(new BodyImage("data/WarriorLeft.gif", 10.5f));
+            isFacingRight = false;
 
         } else if (code == KeyEvent.VK_D) {
             warrior.startWalking(5);
             warrior.removeAllImages();
-            warrior.addImage(new BodyImage("data/warrior.gif", 9.5f));
+            warrior.addImage(new BodyImage("data/warrior.gif", 10.5f));
+            isFacingRight = true;
 
         } else if ( code == KeyEvent.VK_W){
-            warrior.jump(12);
+            warrior.jump(13);
+        }else if ( code == KeyEvent.VK_M  ){
+            if ( isFacingRight ){
+                warrior.removeAllImages();
+                warrior.addImage(new BodyImage("data/warriorstant.png", 21.5f));
+            }else {
+                warrior.removeAllImages();
+                warrior.addImage(new BodyImage("data/warriorstantLeft.png", 21.5f));
+            }
+
         }
 
 
@@ -46,6 +59,10 @@ public class WarriorController implements KeyListener {
             warrior.stopWalking();
         } else if ( code == KeyEvent.VK_D){
             warrior.stopWalking();
+        } else if ( code == KeyEvent.VK_M){
+            warrior.removeAllImages();
+            warrior.addImage(new BodyImage("data/warrior.gif", 10.5f));
+
         }
 
 
