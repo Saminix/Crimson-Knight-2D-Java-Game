2cbd031df89581cd9ff2dc2a199a2638c236e7e6
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-02 17:39:08 +0000

    changing the polygon chape to fit world map, especially when colliding with collisions

--- src/game/Warrior.java
+++ src/game/Warrior.java
@@ -7,13 +7,13 @@ import java.awt.event.KeyListener;
 
 
 public class Warrior extends Walker {
-    private static final Shape warriorShape = new PolygonShape(0.11f,0.6f, 1.81f,0.43f, 1.81f,-2.02f, 0.11f,-4.5f, -3.93f,-3.39f, -2.81f,-0.54f, -0.45f,0.63f);
-    private static final BodyImage image = new BodyImage("data/warrior.gif", 10.5f);
+    private static final Shape warriorShape = new PolygonShape(-1.12f,-2.55f, 0.76f,-2.57f, 1.52f,-0.28f, 1.35f,1.93f, -0.9f,2.33f, -1.47f,0.14f, -1.22f,-2.28f);
+    private static final BodyImage image = new BodyImage("data/HoodedCharacter/HWarriorStop.gif", 7.5f);
 
     private int coins = 0;
     private int score = 0;
 
-    private int health = 100;
+    private int health;
 
     private int coinCount;
 
@@ -21,9 +21,9 @@ public class Warrior extends Walker {
         super(world, warriorShape);
         this.addImage(image);
 
-        coins = 0;
-        score = 0;
-        health = 100;
+        this.coins = 0;
+        this.score = 0;
+        this.health = 100;
 
 
     }
