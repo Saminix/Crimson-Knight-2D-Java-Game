2d2c8cad44b24be900a08e27c554fcaec7508f09
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-02 17:35:04 +0000

    changing dimensions of traps for better accuracy

--- src/game/Trap.java
+++ src/game/Trap.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 import city.cs.engine.DynamicBody;
 
 public class Trap extends StaticBody{
-    private static final Shape trapShape = new CircleShape(1.5f);
+    private static final Shape trapShape = new CircleShape(1.6f);
 
-    private static final BodyImage image = new BodyImage("data/obstacle1.gif", 6f);
+    private static final BodyImage image = new BodyImage("data/obstacle1.gif", 6.4f);
 
 
     public Trap(World world){
