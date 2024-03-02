3749fc01e474901301fc29bbafa495483669781d
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-02 17:37:07 +0000

    Since the dimensions of the player are repeated, such as the walk speed, jump speed and image scale - using private static int/float to the constructor and initalising these values saves time and code duplication. Also a hassle to go back and change values

--- src/game/WarriorController.java
+++ src/game/WarriorController.java
@@ -8,8 +8,16 @@ import java.awt.event.KeyListener;
 
 public class WarriorController implements KeyListener {
     private Warrior warrior;
+    private static final int walkSpeed = 10;
+    private static final int jumpSpeed = 13;
+    private static final float imageScale = 7.5f;
 
-    private boolean isFacingRight = true;
+    private boolean isFacingRight = false;
+
+    // in order to allow the player to move the character in the direction it is facing, to jump or run etc
+
+
+
 
     public WarriorController(Warrior warrior) {
         this.warrior = warrior;
@@ -25,26 +33,38 @@ public class WarriorController implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
         if (code == KeyEvent.VK_A) {
-            warrior.startWalking(-5);
+            warrior.startWalking(-walkSpeed);
             warrior.removeAllImages();
-            warrior.addImage(new BodyImage("data/WarriorLeft.gif", 10.5f));
+            warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorRUNLeft.gif", imageScale));
             isFacingRight = false;
 
         } else if (code == KeyEvent.VK_D) {
-            warrior.startWalking(5);
+            warrior.startWalking(walkSpeed);
             warrior.removeAllImages();
-            warrior.addImage(new BodyImage("data/warrior.gif", 10.5f));
+            warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorRUN.gif", imageScale));
             isFacingRight = true;
 
-        } else if ( code == KeyEvent.VK_W){
-            warrior.jump(13);
+        } else if ( code == KeyEvent.VK_W  ){
+            warrior.jump(jumpSpeed);
+            if ( isFacingRight ){
+                warrior.removeAllImages();
+                warrior.addImage(new BodyImage("data/HoodedCharacter/warriorJump.gif", imageScale-1));
+            }else {
+                warrior.removeAllImages();
+                warrior.addImage(new BodyImage("data/HoodedCharacter/warriorJumpLeft.gif", imageScale-1));
+            }
+
+
+
+
+
         }else if ( code == KeyEvent.VK_M  ){
             if ( isFacingRight ){
                 warrior.removeAllImages();
-                warrior.addImage(new BodyImage("data/warriorstant.png", 21.5f));
+                warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorHit.gif", imageScale));
             }else {
                 warrior.removeAllImages();
-                warrior.addImage(new BodyImage("data/warriorstantLeft.png", 21.5f));
+                warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorHitLeft.gif", imageScale));
             }
 
         }
@@ -55,18 +75,38 @@ public class WarriorController implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
-        if ( code == KeyEvent.VK_A) {
+        if ( code == KeyEvent.VK_A || code == KeyEvent.VK_D) {
             warrior.stopWalking();
-        } else if ( code == KeyEvent.VK_D){
-            warrior.stopWalking();
-        } else if ( code == KeyEvent.VK_M){
+        } else if ( code == KeyEvent.VK_M ){
             warrior.removeAllImages();
-            warrior.addImage(new BodyImage("data/warrior.gif", 10.5f));
+            setWarriorToStop();
+        } else if (code == KeyEvent.VK_W) {
+            warrior.removeAllImages();
+            setWarriorToStop();
 
         }
 
 
     }
+
+
+
+    // method to position which direction the warrior stops in.
+    private void setWarriorToStop(){
+        warrior.removeAllImages();
+        if (isFacingRight) {
+            warrior.removeAllImages();
+            warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorStop.gif", imageScale));
+        } else {
+            warrior.removeAllImages();
+            warrior.addImage(new BodyImage("data/HoodedCharacter/HWarriorStopLeft.gif", imageScale));
+        }
+    }
+
+
+
+
+
 
 
 
