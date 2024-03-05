e7617e0029b0a21eba99b0d4809f42a3182c7545
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-05 16:46:37 +0000

    creating a moving platform that extends staticbody and implements steplistener. using this i added another constructor Vec2 since the coordinates made the platform stay in one x,y vector. Implementing a setPosition using the vec2 position instance will allow me to access this class from gameworld. However a stackoverflow occurs, therefore a super is used to access it from the superclass to the subclass

--- src/game/Trampoline.java
+++ src/game/Trampoline.java
@@ -6,8 +6,50 @@ import org.jbox2d.common.Vec2;
 public class Trampoline extends StaticBody implements StepListener{
 
 
-    private static final Shape trampolineShape = new BoxShape(2,0.2F);
+    private static final Shape trampolineShape = new BoxShape(2.2f,0.2F);
+    private static final BodyImage image = new BodyImage("data/movingPlatform.png", 2.5f);
+    public Vec2 setPosition;
+    private  Vec2 startPosition;
 
+    private GameWorld world;
+    private float top, bottom;
+    private float delta;
+
+    public Trampoline(World w, Vec2 position){
+        super(w, trampolineShape);
+        this.addImage(image);
+        startPosition = position;
+        bottom = startPosition.y;
+        top = startPosition.y+10;
+        delta=0.08f;
+        setPosition(startPosition);
+        w.addStepListener(this);
+
+    }
+
+    @Override
+    public void preStep(StepEvent stepEvent){
+        if (getPosition().y < bottom){
+            this.setPosition(startPosition);
+            delta*=-1;
+
+        }
+        if (getPosition().y > top){
+            delta*=-1;
+        }
+        this.setPosition(new Vec2(this.getPosition().x, this.getPosition().y+delta));
+
+    }
+    @Override
+    public void postStep(StepEvent stepEvent) {
+    }
+
+    public void setPosition(Vec2 position) {
+        super.setPosition(position);
+    }
 }
+
+
+
 
 
