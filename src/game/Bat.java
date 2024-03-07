b1ec52ee463f11e01500e09360509b1963917418
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-07 22:32:54 +0000

    Making the bat move left and right, by using the trampoline techniques for implementing a step listener and changing direction to left and right

--- src/game/Bat.java
+++ src/game/Bat.java
@@ -4,25 +4,26 @@ import city.cs.engine.Shape;
 import org.jbox2d.common.Vec2;
 
 
-public class Bat extends Enemy implements StepListener {
+public class Bat extends Enemy  {
     private static final Shape batShape = new PolygonShape(-0.09f,1.16f, -1.2f,0.78f, -1.2f,0.02f, -0.09f,-0.4f, 0.68f,-0.32f, 1.24f,0.51f, 0.3f,1.16f);
     private static final BodyImage image = new BodyImage("data/bat.gif", 8.5f);
-    private GameWorld world;
+    private GameWorld gameWorld;
     private boolean movingLeft = false;
-    public Vec2 setPosition;
     private  Vec2 startPosition;
     private float left, right;
     private float delta;
 
-    public Bat(World world,Vec2 position, int EnemyHealth, int EnemySpeed, GameWorld gameworld) {
+    public Bat(World world,Vec2 position, int EnemyHealth, int EnemySpeed, GameWorld gameWorld) {
         super(world, batShape,EnemyHealth,EnemySpeed );
         startPosition = position;
         left = startPosition.x - 10;
         right = startPosition.x + 10;
         delta = 0.08f;
         setPosition(startPosition);
+        //makes the bat move(step-listener)
         world.addStepListener(this);
-        this.world = gameworld;
+        //implement access of the game-world for public methods.
+        this.gameWorld = gameWorld;
         this.addImage(image);
         this.setAngularVelocity(0);
         // Disable gravity
@@ -30,25 +31,29 @@ public class Bat extends Enemy implements StepListener {
 
     }
 
+
     @Override
     public void preStep(StepEvent stepEvent) {
         if (getPosition().x <= left || getPosition().x >= right) {
+            // Change direction and update position
             if (getPosition().x <= left) {
                 movingLeft = true;
                 batLeft();
-                this.setPosition(startPosition);
-                 // Reverse the direction of movement when bat is facing left.
-            } else{
+            } else {
                 movingLeft = false;
                 batRight();
-                this.setPosition(startPosition);
-
             }
-            delta *= -1;
+            delta *= -1; // Reverse the direction
         }
         Vec2 newPosition = new Vec2(getPosition().x + delta, getPosition().y);
         setPosition(newPosition);
 
+    }
+
+
+
+    @Override
+    public void postStep(StepEvent stepEvent) {
     }
 
     public void batLeft(){
@@ -60,7 +65,7 @@ public class Bat extends Enemy implements StepListener {
     }
 
     public void batRight(){
-        if(!movingLeft){
+        if(!movingLeft ){
             removeAllImages();
             addImage(new BodyImage("data/bat.gif", 8.5f));
         }
@@ -73,25 +78,17 @@ public class Bat extends Enemy implements StepListener {
     @Override
     public int decrementHealth(int x){ EnemyHealth = EnemyHealth - x;return EnemyHealth;}
 
-    @Override
-    public void chaseWarrior(Warrior warrior) {
-
-    }
 
     @Override
     public void TakenHit(int damage) {
         decrementHealth(damage);
         if (getHealth() <= 0) {
             destroy();
-            Point point = new Point(world);
+            Point point = new Point(this.getWorld(), 2, getPosition().x, getPosition().y);
             point.setPosition(getPosition());
         }
 
 
-    }
-
-    @Override
-    public void postStep(StepEvent stepEvent) {
     }
 
     @Override
