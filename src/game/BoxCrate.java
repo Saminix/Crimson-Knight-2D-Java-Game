bee7eb1bb230153fdf3f93a51d8a0c2714fc157e
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-10 00:29:38 +0000

    Amending the box image

--- src/game/BoxCrate.java
+++ src/game/BoxCrate.java
@@ -1,15 +1,11 @@
 package game;
 import city.cs.engine.*;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
 
-import city.cs.engine.DynamicBody;
 
 public class BoxCrate extends StaticBody {
     private static final Shape boxShape = new BoxShape(2f,2f);
 
-    private static final BodyImage image = new BodyImage("data/BoxCrate.png", 5f);
+    private static final BodyImage image = new BodyImage("data/box.png", 5f);
 
 
     public BoxCrate(World world){
