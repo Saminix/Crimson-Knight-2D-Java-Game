3e624b730edec26d1631ca059c86c3a17d87b9a4
Sameen-Iqbal <157972701+Sameen-Iqbal@users.noreply.github.com>
2024-03-29 17:16:43 +0000

    Adding Wav. files to game. adding audio on loop on game class and when a coin collection takes place

--- src/game/Coins.java
+++ src/game/Coins.java
@@ -1,5 +1,8 @@
 
 package game;
+import java.io.IOException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import city.cs.engine.*;
 import org.jbox2d.common.Vec2;
 
@@ -7,14 +10,38 @@ public class Coins extends Collectibles{
 
 
     private static final String imagePath = "data/Coin.gif";
+    private static SoundClip coinsSound;
+
+    static {
+        try {
+            coinsSound = new SoundClip("audio/coins.wav");
+            System.out.println("coin sound");
+        } catch (UnsupportedAudioFileException|IOException|LineUnavailableException e) {
+            System.out.println(e);
+        }
+    }
+
 
     public Coins(World world, float radius,float x, float y){
         super(world, new CircleShape(radius), new Vec2(x,y));
         float height = 6;
         addImage(new BodyImage(imagePath, height));
+
     }
 
 
+    @Override
+    public void destroy()
+
+    {
+        coinsSound.play();
+        coinsSound.setVolume(0.5);
+        super.destroy();
+    }
+
+    public void setVolume(double volume){
+
+    }
 
 
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -1,9 +1,10 @@
 package game;
 
 import city.cs.engine.*;
-import org.jbox2d.common.Vec2;
 
-
+import java.io.IOException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 
@@ -14,11 +15,16 @@ import javax.swing.*;
 public class Game {
 
     GameLevel currentLevel;
+    private JFrame frame;
 
     /** A graphical display of the world (a specialised JPanel). */
     GameView view;
 
+    private SoundClip gameMusic;
+
     WarriorController controller;
+
+    Warrior warrior;
 
 
 
@@ -26,8 +32,33 @@ public class Game {
 
     /** Initialise a new Game. */
     public Game() {
+        MainMenu menu = new MainMenu(this);
+
+        try {
+            gameMusic = new SoundClip("audio/Birds.wav");
+            gameMusic.loop();
+        }catch (UnsupportedAudioFileException|IOException|LineUnavailableException e) {
+            System.out.println(e);
+        }
+
+
+        if(menu.IsPlayButtonPressed()){
+            System.out.println("button pressed");
+            startGame();
+        }
+
+
+
+
+    }
+
+
+    public void startGame(){
+
 
         currentLevel = new Level1(this);
+
+
 
 
         //3. make a view to look into the game world
@@ -45,6 +76,7 @@ public class Game {
         // view.setGridResolution(1);
 
 
+
         //4. create a Java window (frame) and add the game
         //   view to it
         final JFrame frame = new JFrame("City Game");
@@ -53,6 +85,7 @@ public class Game {
         // when the x button is pressed
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
+        frame.setLocationRelativeTo(null);
         // don't let the frame be resized
         frame.setResizable(false);
         // size the frame to fit the world view
@@ -63,14 +96,32 @@ public class Game {
 
         //optional: uncomment this to make a debugging view
         JFrame debugView = new DebugViewer(currentLevel, 800, 600);
-
         // start our game world simulation!
         view.requestFocus();
         currentLevel.start();
+    }
+
+
+
+
+
+    public void isNextLevel(){
+        LevelPanel nextLevel = new LevelPanel(this);
+        if(nextLevel.IsButtonPressed()){
+            goToNextLevel();
+        }
+
 
     }
 
+
+
+
     public void goToNextLevel(){
+
+
+
+
 
         System.out.println("Yes, lets go to next level");
 
@@ -78,16 +129,22 @@ public class Game {
 
             currentLevel.stop();
 
-            Warrior prevStudent = currentLevel.getWarrior();
-
             currentLevel = new Level2(this);
+            Warrior newWarrior = currentLevel.getWarrior();
+            JFrame debugView = new DebugViewer(currentLevel, 800, 600);
 
-            Warrior newStudent = currentLevel.getWarrior();
+            currentLevel.addStepListener(new Tracker(view, currentLevel.getWarrior()));
+            view.setCentre(currentLevel.getWarrior().getPosition());
+
 
             view.setWorld(currentLevel);
-            view.setBackgroundImage("data/dessert.jpg");
-
             controller.updateWarrior(currentLevel.getWarrior());
+           
+            view.setBackgroundImage("data/dessesrt.jpg");
+
+            currentLevel.getWarrior().ResetAttributes();
+
+
 
 
             currentLevel.start();
@@ -96,6 +153,9 @@ public class Game {
             System.out.println("Game done");
             System.exit(0);
         }
+
+
+
 
 
     }
